a61e969b021db3ea288e0a10311acd6fed07aff3
Lukas Schumacher <lu.schumacher@bluewin.ch>
2022-03-29 16:02:00 +0200

    .

--- src/fast_dm_simulator.py
+++ src/fast_dm_simulator.py
@@ -2,20 +2,14 @@ import numpy as np
 from numba import njit
 from generative_models import diffusion_trial
 
-from tensorflow.keras.utils import to_categorical
+def fast_dm_simulate(params, context):
+    n_obs = context.shape[0]
+    pred_rt = np.empty(n_obs)
+    for t in range(n_obs):
+        drift = np.random.normal(params[context[t]], params[6])
+        ndt = np.random.uniform(params[5] - params[7]/2, params[5] + params[7]/2)
 
-@njit
-def fast_dm_simulate(trials, theta, context):
-    n_subs = theta.shape[0]
-    pred_rt = np.empty((n_subs, trials))
-    for n in range(n_subs):
-        params = theta.iloc[n, 1:]
-
-        for t in range(trials):
-            drift = np.random.normal(theta[context[t]], theta[6])
-            ndt = np.random.uniform(theta[5] - theta[7]/2, theta[5] + theta[7]/2)
-
-            pred_rt[n, t] = diffusion_trial(drift, theta[4], ndt)
+        pred_rt[t] = diffusion_trial(drift, params[4], ndt)
 
     return pred_rt
 
